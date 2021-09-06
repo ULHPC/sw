d3284d8e1782f122d62d00aeca33d820b72c9a2b
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2021-09-06 09:17:31 +0200

    [settings] 2020b,2021a update
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

---
+++ settings/2021a/aion.sh
@@ -0,0 +1,22 @@
+# Usage: source settings/<version>/aion.sh
+#
+# Example for interactive test 
+#   ./scripts/get-interactive-job
+#    source settings/<version>/aion.sh
+#    echo $EASYBUILD_PREFIX
+
+TOP_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && git rev-parse --show-toplevel)"
+
+export LOCAL_RESIF_ENVIRONMENT=2021a
+
+if [ -f "${TOP_DIR}/settings/aion.sh" ]; then
+    . ${TOP_DIR}/settings/aion.sh
+fi
+if [[ "$(whoami)" == 'resif' ]]; then
+    cat <<EOF
+/!\\ WARNING
+/!\\ WARNING: NEVER run test builds as resif '$(whoami)'
+/!\\ WARNING
+EOF
+    exit 1
+fi
